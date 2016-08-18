61e5c1b45f6452955286a089cedba5b6ad561e53
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-08-17 21:16:58 -0300

    Segundo commit

---
+++ Jam365Indies/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public int sceneToLoadID;
+
+
+	public void ChangeScene()
+	{
+		SceneManager.LoadScene(sceneToLoadID);
+	}
+}
