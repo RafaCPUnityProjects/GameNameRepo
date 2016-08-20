914cd5aa875f1c1667e5f9d8229c3aa18a486514
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-08-20 08:25:06 -0300

    Importadas imagens dos outros legumes no projeto Criados prefabs para cada legume Criado script instanciador dos legumes

---
+++ Jam365Indies/Assets/Scripts/Gameplay/VegetableFieldSpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VegetableFieldSpawner : MonoBehaviour
+{
+	public GameObject[] vegetablesPrefabs;
+	public Transform sizeMarker;
+	public int vegetablePerRoll;
+	public int vegetablePerCollum;
+
+	private Vector3 fieldDimensions;
+	private Vector2 fieldStepValues;
+	private Vector3 startPos;
+	private GameObject[,] plantedVegetables;
+
+	void Start()
+	{
+		Initialize();
+		PlantVegetables();
+	}
+
+	private void PlantVegetables()
+	{
+		Vector3 pos;
+		for (int y = 0; y < vegetablePerCollum; y++)
+		{
+			for (int x = 0; x < vegetablePerRoll; x++)
+			{
+				pos = new Vector3(fieldStepValues.x * x, 0, fieldStepValues.y * y);
+				plantedVegetables[x, y] = (GameObject)Instantiate(GetRandomVegetable(), startPos + pos, Quaternion.identity);
+			}
+		}
+	}
+
+	private void Initialize()
+	{
+		startPos = transform.position;
+
+		fieldDimensions = sizeMarker.position - startPos;
+
+		float xStep = fieldDimensions.x / vegetablePerRoll;
+		float yStep = fieldDimensions.z / vegetablePerCollum;
+		fieldStepValues = new Vector2(xStep, yStep);
+
+		plantedVegetables = new GameObject[vegetablePerRoll, vegetablePerCollum];
+	}
+
+	GameObject GetRandomVegetable()
+	{
+		return vegetablesPrefabs[Random.Range(0, vegetablesPrefabs.Length)];
+	}
+}
