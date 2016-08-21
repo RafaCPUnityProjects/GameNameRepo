47c3c7ca66a0183f2ca24c187552334470aebc0e
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-08-21 17:54:55 -0300

    Algumas várias coisas, incluindo overlay de escuridão, camera como filha do player (o player follow mais barato que tem)

---
+++ Jam365Indies/Assets/Scripts/Gameplay/DeathTimer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTimer : MonoBehaviour
+{
+	public float deathTimer;
+
+	void Start()
+	{
+		Destroy(this.gameObject, deathTimer);
+	}
+}
