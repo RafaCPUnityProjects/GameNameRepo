d70f1b43d074d4da69d81e845713ce3bc27a8bb4
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-08-21 20:38:58 -0300

    Som de passos e attack do player. Quando voltar coloco os outros sons

--- Jam365Indies/Assets/Scripts/Gameplay/PlayerMovement.cs
+++ Jam365Indies/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -14,68 +14,68 @@ public class PlayerMovement : MonoBehaviour
 	private bool isAttacking = false;
 	private float slashTimer = 0;
 
+	[FMODUnity.EventRef]
+	public string PlayerWalk;
+	[FMODUnity.EventRef]
+	public string PlayerAttack;
+
+	FMOD.Studio.EventInstance walk;
+
 	// Use this for initialization
 	void Start()
 	{
 		rb2D = GetComponent<Rigidbody2D>();
 		animator = GetComponentInChildren<Animator>();
-		slash.SetActive (false);
+		slash.SetActive(false);
+		walk = FMODUnity.RuntimeManager.CreateInstance(PlayerWalk);
+		walk.start();
+		walk.setParameterValue("speed", 0);
+	}
+
+	void OnDestroy()
+	{
+		if (walk != null)
+		{
+			walk.release();
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
-		if (isAttacking) {
+		if (isAttacking)
+		{
 			slashTimer -= Time.deltaTime;
-			if (slashTimer < 0) {
+			if (slashTimer < 0)
+			{
 				isAttacking = false;
-				slash.SetActive (false);
+				slash.SetActive(false);
 			}
 		}
 
 		inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		if (Input.GetButtonDown("Fire1") && !isAttacking )
+		if (Input.GetButtonDown("Fire1") && !isAttacking)
 		{
 			animator.SetTrigger("Attack");
+			inputVector = Vector2.zero;
+			FMODUnity.RuntimeManager.PlayOneShot(PlayerAttack, transform.position);
 			Attack();
 		}
 
-		if (!isAttacking) 
+		if (!isAttacking)
 		{
 			Vector3 moveVector = transform.position + new Vector3(inputVector.x, inputVector.y) * Time.deltaTime * moveSpeed;
 			rb2D.MovePosition(moveVector);
 			animator.SetFloat("Horizontal", inputVector.x);
 			animator.SetFloat("Vertical", inputVector.y);
 		}
+		walk.setParameterValue("speed", inputVector != Vector2.zero ? 1 : 0);
 	}
 
 	private void Attack()
 	{
 		isAttacking = true;
-		slash.SetActive (true);
+		slash.SetActive(true);
 		slashTimer = attackTime;
-
-
-		if(Mathf.Abs(inputVector.x) > Mathf.Abs(inputVector.y)) //horitontal
-		{
-			if(inputVector.x > 0) //right
-			{
-
-			}else //left
-			{
-
-			}
-		}else //vertical
-		{
-			if (inputVector.y > 0) //up
-			{
-
-			}
-			else //down
-			{
-
-			}
-		}
 	}
 }
